147f7aab62077bed199bcc58105a75b92743ce4e
andrii_manzar <manzar20011@gmail.com>
2022-08-29 22:07:40 +0300

    added util class for password encrypt

---
+++ src/main/java/com/manzar/util/BCryptUtil.java
@@ -0,0 +1,17 @@
+package com.manzar.util;
+
+import com.password4j.Hash;
+import com.password4j.Password;
+
+public class BCryptUtil {
+
+    public static String encrypt(String input) {
+        Hash hash = Password.hash(input).withBcrypt();
+        return hash.getResult();
+    }
+
+    public static boolean comparePasswords(String userProvidedPassword, String hashFromDb){
+        return Password.check(userProvidedPassword, hashFromDb).withBcrypt();
+    }
+}
+
