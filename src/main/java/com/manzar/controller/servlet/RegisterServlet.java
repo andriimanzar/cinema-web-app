c1881a493498215594dbd4002cd0dcb86feee627
andrii_manzar <manzar20011@gmail.com>
2022-08-30 23:00:42 +0300

    added RegisterServlet, updated pom.xml

---
+++ src/main/java/com/manzar/controller/servlet/RegisterServlet.java
@@ -0,0 +1,54 @@
+package com.manzar.controller.servlet;
+
+import com.manzar.persistence.DAO.UserDao;
+import com.manzar.persistence.DAO.UserDaoImpl;
+import com.manzar.persistence.entity.User;
+import com.manzar.util.BCryptUtil;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    private UserDao userDao;
+
+    @Override
+    public void init() {
+        userDao = UserDaoImpl.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/registration.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(!req.getParameter("password").equals(req.getParameter("repeatedPassword"))) {
+            resp.sendRedirect("/register");
+
+
+        }
+        else {
+            User newUser = parseUserFromRequest(req);
+            userDao.save(newUser);
+        }
+    }
+
+    private User parseUserFromRequest(HttpServletRequest req){
+
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String email = req.getParameter("email");
+        String phoneNumber = req.getParameter("phoneNumber");
+        String encryptedPassword = BCryptUtil.encrypt(req.getParameter("password"));
+        return new User(firstName, lastName, email, phoneNumber, encryptedPassword);
+    }
+
+}
